c9dde074f7c447fd43fb2cd583dc70513d211da8
Fred Malouf <fred.malouf@nio.io>
2020-02-06 11:16:11 -0800

    Move version for QNX additions

--- conanfile.py
+++ conanfile.py
@@ -8,7 +8,7 @@ from conans.errors import ConanInvalidConfiguration
 
 class LibuvConan(ConanFile):
     name = "libuv"
-    version = "1.30.1"
+    version = "1.30.2"
     description = "Cross-platform asynchronous I/O "
     url = "https://github.com/maingig/libuv.git"
     homepage = "https://github.com/libuv/libuv"
